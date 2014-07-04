7f1cdea5e47713e284abfd2462b8014d8e931559
truonglx-56 <truonglx.5293@gmail.com>
2014-07-04 21:41:46 +0700

    Lab1
    Nop bai tap thuc hanh tuan 1

---
+++ Beer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beer;
+
+/**
+ *
+ * @author TruongLX
+ */
+public class Beer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           System.out.print("Ho va ten: Le Xuan Truong \t");
+            System.out.print("Ma sv: 11020355 \t");
+            System.out.print("Lop: K56CD \t");
+            System.out.println("User name: truonglx_56");
+            for(int i=1;i<=20;i++){
+            System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            System.out.println("...");
+            System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+            System.out.println("Take one down, pass it around,");
+            System.out.println("No more bottles of beer on the wall.");
+         
+    }
+    
+}
+}
